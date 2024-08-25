452c56d026d9bf51404c7665002bf3223e07c161
ThinDrew <andreynikonorov5@gmail.com>
2024-08-25 10:47:33 +0700

    Создал кнопку реагирующую на сигнал connect, disconnect

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -2,14 +2,24 @@ import React, { Component } from "react";
 
 interface IMainPageState {
     count: number;
+    isConnect: boolean;
 }
 
 export default class MainPage extends Component<{}, IMainPageState>{
+    timerId: any;
+
     constructor (props: any){
         super(props);
         this.state = {
-            count: 0
+            count: 0,
+            isConnect: false
         }
+    }
+
+    connect(e: any){
+        this.setState((state) => ({
+            isConnect: !state.isConnect
+        }));
     }
 
     incCounter(e: any) {
@@ -18,11 +28,22 @@ export default class MainPage extends Component<{}, IMainPageState>{
         }))
     }
 
+    componentDidMount(): void {
+        this.timerId = setInterval(
+            (e: any) => this.incCounter(e),
+            1000
+        )
+    }
+
+    componentWillUnmount(): void {
+        clearInterval(this.timerId);
+    }
+
     render(): React.ReactNode {
         return(
             <div>
-                <input type="button" value="Connect" onClick={(e) => this.incCounter(e)}/>
-                <input type="button" value="Send"/>
+                <input type="button" value={this.state.isConnect? "Disconnect" : "Connect"} onClick={(e) => this.connect(e)}/>
+                <input type="button" value="Send" disabled={!this.state.isConnect}/>
                 <p>message: {this.state.count}</p>
                 <p>status:</p>
                 <p>text:</p>
