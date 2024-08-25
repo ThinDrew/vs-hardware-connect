7c75b2d4cb44809eb03fe03001d9445841d891f8
ThinDrew <andreynikonorov5@gmail.com>
2024-08-25 11:55:48 +0700

    Ошибка веб сокета

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
+import WSControl from "./controllers/devices/ws/client/wscontroller";
+import { IErrorMessage } from "./interfaces/IErrorMessage";
+import { IServiceRespond } from "./interfaces/IServiceRespond";
+import { validationJSON } from "./lib/util/errors";
 
 interface IMainPageState {
     count: number;
     isConnect: boolean;
 }
 
+const HOST: string = window.location.hostname;
+const port: number = 5000;
+const URL: string = `${HOST}:${port}`;
+const urlService: string = `ws://${URL}`;
+
 export default class MainPage extends Component<{}, IMainPageState>{
     timerId: any;
+    private wss: WSControl | null = null;
 
     constructor (props: any){
         super(props);
@@ -16,10 +26,32 @@ export default class MainPage extends Component<{}, IMainPageState>{
         }
     }
 
-    connect(e: any){
-        this.setState((state) => ({
-            isConnect: !state.isConnect
-        }));
+    public validateIncomingMessage(respond: any): IServiceRespond | IErrorMessage {
+        let msg: any = validationJSON(respond);
+        return msg;
+    }
+
+    private async connect() {
+        this.wss = new WSControl({host: urlService});
+        const respond: any = await this.wss!.open();
+        const msg: any = this.validateIncomingMessage(respond);
+    }
+
+    private disconnect() {
+
+    }
+
+    private async socketControl(e: any){
+        // this.setState((state) => ({
+        //     isConnect: !state.isConnect
+        // }));
+        if (this.state.isConnect){
+            this.disconnect()
+        }
+        else {
+            await this.connect()
+        }
+        
     }
 
     incCounter(e: any) {
@@ -42,7 +74,7 @@ export default class MainPage extends Component<{}, IMainPageState>{
     render(): React.ReactNode {
         return(
             <div>
-                <input type="button" value={this.state.isConnect? "Disconnect" : "Connect"} onClick={(e) => this.connect(e)}/>
+                <input type="button" value={this.state.isConnect? "Disconnect" : "Connect"} onClick={async(e) => await this.socketControl(e)}/>
                 <input type="button" value="Send" disabled={!this.state.isConnect}/>
                 <p>message: {this.state.count}</p>
                 <p>status:</p>
